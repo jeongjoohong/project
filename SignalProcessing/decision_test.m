eba4ed2ef70fc99711019960048d5896011767a9
Jeongjoo Hong <jeongjoohong@users.noreply.github.com>
2017-04-19 13:14:57 +0900

    신호처리 시뮬레이션 코드 수정
    코드 정리, figure 변경

--- SignalProcessing/decision_test.m
+++ SignalProcessing/decision_test.m
@@ -2,21 +2,25 @@ close all;
 clear;
 clc;
 
+bandw = 0.1;
+freq1 = 50;
+freq2 = 120;
+amp1 = 0.6;
+amp2 = 0.8;
+plotCol = 7;
+plotInd = 0;
+
 Fs = 400;       % Sampling frequency
 T = 1/Fs;       % Sampling period
 L = 400;        % Length of signal
 t = (0:L-1)*T;  % Time vector
 
-width = 0.5;
-freq1 = 50;
-freq2 = 120;
-amp1 = 0.7;
-amp2 = 0.9;
-
-S = amp1*sin(2*pi*freq1*t)+amp2*sin(2*pi*freq2*t);
+S = amp1*sin(2*pi*50*t)+amp2*sin(2*pi*120*t);
 X = S+randn(size(t));
+
 figure('units','normalized','outerposition',[0 0 1 1]);
-subplot(5,2,1);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
 plot(1000*t,X);
 title('Signal with Noise');
 xlabel('t (milliseconds)');
@@ -24,62 +28,94 @@ ylabel('X(t)');
 
 Y = fftshift(fft(X,L));
 fY = (-numel(Y)/2:numel(Y)/2-1);
-subplot(5,2,2);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
 plot(fY, abs(Y));
 title('FFT of X(t)');
 xlabel('f (Hz)');
 ylabel('|X(f)|');
 
-A1 = sinc(width*(t-0.5)) .* cos(2*pi*freq1*t);
-subplot(5,2,3);
+A1 = sinc(bandw*(t-0.5)) .* cos(2*pi*freq1*t);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
 plot(1000*t,A1);
-title(sprintf('Sinc (%.1fHz, centered, shifted %dHz)',width,freq1));
+title(sprintf('Sinc (%.1fHz, centered, shifted %dHz)',bandw,freq1));
 xlabel('t (milliseconds)');
-ylabel(sprintf('sinc(%.1f*(t-0.5)) * cos(2*pi*%d*t)',width,freq1));
+ylabel(sprintf('sinc(%.1f*(t-0.5)) * cos(2*pi*%d*t)',bandw,freq1));
 
-A2 = sinc(width*(t-0.5)) .* cos(2*pi*freq2*t);
-subplot(5,2,4);
+A2 = sinc(bandw*(t-0.5)) .* cos(2*pi*freq2*t);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
 plot(1000*t,A2);
-title(sprintf('Sinc (%.1fHz, centered, shifted %dHz)',width,freq2));
+title(sprintf('Sinc (%.1fHz, centered, shifted %dHz)',bandw,freq2));
 xlabel('t (milliseconds)');
-ylabel(sprintf('sinc(%.1f*(t-0.5)) * cos(2*pi*%d*t)',width,freq2));
+ylabel(sprintf('sinc(%.1f*(t-0.5)) * cos(2*pi*%d*t)',bandw,freq2));
 
 B1 = fftshift(fft(A1,L));
 fB1 = (-numel(B1)/2:numel(B1)/2-1);
-subplot(5,2,5);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
 plot(fB1, abs(B1));
 title('FFT of sinc(t)');
 xlabel('f (Hz)');
 
 B2 = fftshift(fft(A2,L));
 fB2 = (-numel(B2)/2:numel(B2)/2-1);
-subplot(5,2,6);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
 plot(fB2, abs(B2));
 title('FFT of sinc(t)');
 xlabel('f (Hz)');
 
 C1 = ifft(ifftshift(B1),L);
-subplot(5,2,7);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
 plot(1000*t,C1);
 title('IFFT of sinc(f)');
 xlabel('t (milliseconds)');
 
 C2 = ifft(ifftshift(B2),L);
-subplot(5,2,8);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
 plot(1000*t,C2);
 title('IFFT of sinc(f)');
 xlabel('t (milliseconds)');
 
 D1 = conv(X,C1);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
+plot(D1);
+title('Convolution');
+
 D2 = conv(X,C2);
-E1 = [max(abs(D1)) max(abs(D2))];
-E2 = [E1 ; E1*0];
-subplot(5,1,5);
-b = bar(E2, 'FaceColor', [0 0 1]);
-title(sprintf('Compare with Convolution %dHz and %dHz',freq1,freq2));
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
+plot(D2);
+title('Convolution');
+
+D1 = conv(X,C1);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
+plot(abs(D1));
+title('Absolute of Convolution');
+
+D2 = conv(X,C2);
+plotInd = plotInd+1;
+subplot(plotCol,2,plotInd);
+plot(abs(D2));
+title('Absolute of Convolution');
+
+E = [max(abs(D1)) max(abs(D2))];
+plotInd = plotInd+1;
+subplot(plotCol,1,ceil(plotInd/2));
+hold on
+for i = 1 : length(E)
+    b(i) = bar(i, E(i));
+end
+hold off
+title(sprintf('Maximum Absolute Value of Convolution %dHz, and %dHz',freq1,freq2));
 ylabel('max(|conv(X(t), sinc(t))|)');
-xlim([0.72 1.28]);
-set(gca, 'XTick', []);
+set(gca, 'XTickLabel',E,'XTick',1:length(E))
 if max(abs(D1)) > max(abs(D2))
     b(1).LineWidth = 2;
     b(1).EdgeColor = 'red';
