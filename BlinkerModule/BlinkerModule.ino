881d3a2f40746f6734904cd24ccf2549bbd81671
Jeongjoo Hong <jeongjoohong@users.noreply.github.com>
2017-04-27 17:52:12 +0900

    블링커 모듈 수정
    세그먼트 깜빡임 문제 해결을 위한 라이브러리 추가, 코드 정리, 옵션 추가(최대 깜빡임 주파수에서는 LED를 켬)

--- BlinkerModule/BlinkerModule.ino
+++ BlinkerModule/BlinkerModule.ino
@@ -1,45 +1,48 @@
 #include "OneEuroFilter.h"
+#include "TimerOne.h"
 
 #define VOLUME_L    A0
 #define VOLUME_R    A1
-#define VOLUME_C    A2
+#define VOLUME_T    A2
 
 #define LED_R       12
 #define LED_L       11
-
 #define POT_SELECT  10
 #define POT_CLOCK   9
 #define POT_MOSI    8
-
-#define REG_DATA    7 // DIGITAL(PWM~)
+#define REG_DATA    7
 #define REG_LATCH   6
 #define REG_CLOCK   5
 
 #define MILLISECONDS  1000L
 #define MICROSECONDS  1000000L
 
-const bool isMicrosecond    = true;
-const bool isDebugging      = true;
-const bool isFiltering      = false;
-const float ledDutyCycle    = 0.5;
-const double minCutoff      = 1.0;
-const double beta           = 0.007;
-const double inputFrequency = 50;
+const bool isMicrosecond  = true;
+const bool isDebugging    = true;
+const bool isFiltering    = true;
 
-unsigned long long updatePrevious;
-OneEuroFilter channel1(inputFrequency, minCutoff, beta);
-OneEuroFilter channel2(inputFrequency, minCutoff, beta);
+const double samplingFrequency = 1000;
+const double minCutoff  = 1.0;
+const double beta       = 0.007;
 
-short level1;
-short level2;
-short level3;
-short number[4];
-unsigned long long level1Previous;
-unsigned long long level2Previous;
-unsigned long long level3Previous;
-bool isLed1On;
-bool isLed2On;
-bool isUpdated;
+const float maxBlinkFrequency = 30;
+const float maxDigipotLevel   = 256;
+const float ledDutyCycle      = 0.5;
+
+OneEuroFilter channel1(samplingFrequency, minCutoff, beta);
+OneEuroFilter channel2(samplingFrequency, minCutoff, beta);
+OneEuroFilter channel3(samplingFrequency, minCutoff, beta);
+
+unsigned long long previousRead;
+unsigned long long previousBlinkL;
+unsigned long long previousBlinkR;
+
+short volume1;
+short volume2;
+short volume3;
+short segment[4];
+bool isLedLOn;
+bool isLedROn;
 unsigned long long segmentIndex;
 
 void setup() {
@@ -53,120 +56,142 @@ void setup() {
   pinMode(LED_L, OUTPUT);
   pinMode(LED_R, OUTPUT);
 
-  updatePrevious = 0;
-  level1Previous = 0;
-  level2Previous = 0;
-  level3Previous = 0;
-  isLed1On = false;
-  isLed2On = false;
-  isUpdated = false;
+  previousRead = 0;
+  previousBlinkL = 0;
+  previousBlinkR = 0;  
+  isLedLOn = false;
+  isLedROn = false;
   segmentIndex = 0;
 
   registerBlink(2, 500);
+
+  Timer1.initialize(500);
+  Timer1.attachInterrupt(displaySegment);
 }
 
 void loop() {
-  unsigned long long current = isMicrosecond ? micros() : millis();
-  if (current - updatePrevious >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / inputFrequency) {
-    updatePrevious = current;
-    short received1 = analogRead(VOLUME_L);
-    short received2 = analogRead(VOLUME_R);
-    short received3 = analogRead(VOLUME_C);
-    short filtered1;
-    short filtered2;
-
-    isUpdated = true;
-
+  unsigned long long currentTimestamp = isMicrosecond ? micros() : millis();
+  if (currentTimestamp - previousRead >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / samplingFrequency) {
+    previousRead = currentTimestamp;
+    short input1 = analogRead(VOLUME_L);
+    short input2 = analogRead(VOLUME_R);
+    short input3 = analogRead(VOLUME_T);
+    short filter1;
+    short filter2;
+    short filter3;
+    
     if (isFiltering) {
-      filtered1 = round(channel1.filter(received1));
-      filtered2 = round(channel2.filter(received2));
-      level1 = round(filtered1 * (30.0 / 1024.0));
-      level2 = round(filtered2 * (30.0 / 1024.0));
-      level3 = round(received3 * (256.0 / 1024.0));
+      filter1 = round(channel1.filter(input1));
+      filter2 = round(channel2.filter(input2));
+      filter3 = round(channel3.filter(input3));
+      
+      volume1 = round(filter1 * (maxBlinkFrequency / 1024.0));
+      volume2 = round(filter2 * (maxBlinkFrequency / 1024.0));
+      segment[0] = volume1 / 10;
+      segment[1] = volume1 % 10;
+      segment[2] = volume2 / 10;
+      segment[3] = volume2 % 10;
+      
+      short digipotLevel = round(filter3 * (maxDigipotLevel / 1024.0));
+      if (volume3 != digipotLevel) {
+        volume3 = digipotLevel;
+        digitalWrite(POT_SELECT, LOW);
+        shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, B00000000);
+        shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, volume3);
+        digitalWrite(POT_SELECT, HIGH);
+        digitalWrite(POT_SELECT, LOW);
+        shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, B00010000);
+        shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, volume3);
+        digitalWrite(POT_SELECT, HIGH);
+      }
     } else {
-      level1 = round(received1 * (30.0 / 1024.0));
-      level2 = round(received2 * (30.0 / 1024.0));
-      level3 = round(received3 * (256.0 / 1024.0));
+      volume1 = round(input1 * (maxBlinkFrequency / 1024.0));
+      volume2 = round(input2 * (maxBlinkFrequency / 1024.0));
+      segment[0] = volume1 / 10;
+      segment[1] = volume1 % 10;
+      segment[2] = volume2 / 10;
+      segment[3] = volume2 % 10;
+      
+      short digipotLevel = round(input3 * (maxDigipotLevel / 1024.0));
+      if (volume3 != digipotLevel) {
+        volume3 = digipotLevel;
+        digitalWrite(POT_SELECT, LOW);
+        shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, B00000000);
+        shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, volume3);
+        digitalWrite(POT_SELECT, HIGH);
+        digitalWrite(POT_SELECT, LOW);
+        shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, B00010000);
+        shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, volume3);
+        digitalWrite(POT_SELECT, HIGH);
+      }
     }
-
+    
     if (isDebugging) {
-      Serial.print(received1);
+      Serial.print(input1);
+      Serial.print("\t");
+      Serial.print(input2);
       Serial.print('\t');
-      Serial.print(received2);
-      Serial.print('\t');
-      Serial.print(received3);
+      Serial.print(input3);
       if (isFiltering) {
         Serial.print('\t');
-        Serial.print(filtered1);
+        Serial.print(filter1);
         Serial.print('\t');
-        Serial.print(filtered2);
+        Serial.print(filter2);
         Serial.print('\t');
-        Serial.print(received2);
+        Serial.print(filter3);
       }
       Serial.print('\n');
     }
-    
-    number[0] = level1 / 10;
-    number[1] = level1 % 10;
-    number[2] = level2 / 10;
-    number[3] = level2 % 10;
   }
 
-  current = isMicrosecond ? micros() : millis();
-  if (level1 == 0) {
-    isLed1On = false;
-    digitalWrite(LED_L, LOW);
+  currentTimestamp = isMicrosecond ? micros() : millis();
+  if (volume1 == 0) {
+    if (isLedLOn) {
+      isLedLOn = false;
+      digitalWrite(LED_L, LOW);
+    }
+  } else if (volume1 == maxBlinkFrequency) {
+    if (!isLedLOn) {
+      isLedLOn = true;
+      digitalWrite(LED_L, HIGH);
+    }
   } else {
-    if (current - level1Previous >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / level1) {
-      level1Previous = current;
-      isLed1On = true;
+    if (currentTimestamp - previousBlinkL >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / volume1) {
+      previousBlinkL = currentTimestamp;
+      isLedLOn = true;
       digitalWrite(LED_L, HIGH);
     } else {
-      if (current - level1Previous >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / level1 * ledDutyCycle) {
-        if (isLed1On == true) {
-          isLed1On = false;
+      if (currentTimestamp - previousBlinkL >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / volume1 * ledDutyCycle) {
+        if (isLedLOn == true) {
+          isLedLOn = false;
           digitalWrite(LED_L, LOW);
         }
       }
     }
   }
-  if (level2 == 0) {
-    isLed2On = false;
-    digitalWrite(LED_R, isLed2On);
+  if (volume2 == 0) {
+    if (isLedROn) {
+      isLedROn = false;
+      digitalWrite(LED_R, isLedROn);
+    }
+  } else if (volume2 == maxBlinkFrequency) {
+    if (!isLedROn) {
+      isLedROn = true;
+      digitalWrite(LED_R, HIGH);
+    }
   } else {
-    if (current - level2Previous >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / level2) {
-      level2Previous = current;
-      isLed2On = true;
+    if (currentTimestamp - previousBlinkR >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / volume2) {
+      previousBlinkR = currentTimestamp;
+      isLedROn = true;
       digitalWrite(LED_R, HIGH);
     } else {
-      if (current - level2Previous >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / level2 * ledDutyCycle) {
-        if (isLed2On == true) {
-          isLed2On = false;
+      if (currentTimestamp - previousBlinkR >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / volume2 * ledDutyCycle) {
+        if (isLedROn == true) {
+          isLedROn = false;
           digitalWrite(LED_R, LOW);
         }
       }
     }
-  }
-  if (isUpdated) {
-    isUpdated = false;
-    digitalWrite(POT_SELECT, LOW);
-    shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, B00000000);
-    shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, level3);
-    digitalWrite(POT_SELECT, HIGH);
-    digitalWrite(POT_SELECT, LOW);
-    shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, B00010000);
-    shiftOut(POT_MOSI, POT_CLOCK, MSBFIRST, level3);
-    digitalWrite(POT_SELECT, HIGH);
-  }
-
-  {
-    int index = segmentIndex % 4;
-    byte segment = (B1111 ^ (1 << ((sizeof(number) / sizeof(int) - 1) - index)));
-    byte data = (segment << 4) + number[index];
-    digitalWrite(REG_LATCH, LOW);
-    shiftOut(REG_DATA, REG_CLOCK, MSBFIRST, data);
-    digitalWrite(REG_LATCH, HIGH);
-    segmentIndex++;
   }
 }
 
@@ -185,5 +210,15 @@ void registerBlink(int times, int duration) {
     digitalWrite(REG_LATCH, HIGH);
     delay(duration);
   }
+}
+
+void displaySegment() {
+  int index = segmentIndex % 4;
+  byte sequence = (B1111 ^ (1 << ((sizeof(segment) / sizeof(int) - 1) - index)));
+  byte data = (sequence << 4) + segment[index];
+  digitalWrite(REG_LATCH, LOW);
+  shiftOut(REG_DATA, REG_CLOCK, MSBFIRST, data);
+  digitalWrite(REG_LATCH, HIGH);
+  segmentIndex++;
 }
 
