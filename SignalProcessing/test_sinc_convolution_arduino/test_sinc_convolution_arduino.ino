31151ac4416ffb8ebb329229fd9829b816a97766
Jeongjoo Hong <jeongjoohong@users.noreply.github.com>
2017-05-12 18:35:55 +0900

    신호처리 시뮬레이션 코드 수정
    코드 정리

--- SignalProcessing/test_sinc_convolution_arduino/test_sinc_convolution_arduino.ino
+++ SignalProcessing/test_sinc_convolution_arduino/test_sinc_convolution_arduino.ino
@@ -3,55 +3,55 @@
 #define MILLISECONDS  1000L
 #define MICROSECONDS  1000000L
 
-#define BUFFER_SIZE   18
+#define BUFFER_SIZE   25
 
 const bool isMicrosecond  = true;
 const float samplingFrequency = 60;
-unsigned long previousRead;
+unsigned long lastTimestamp;
 
-float inputs[BUFFER_SIZE] = {};
-float sincF1[BUFFER_SIZE] = {};
-float sincF2[BUFFER_SIZE] = {};
-int inputsLength = sizeof(inputs) / sizeof(float);
-int sincF1Length = sizeof(sincF1) / sizeof(float);
-int sincF2Length = sizeof(sincF2) / sizeof(float);
+const unsigned int inputsLength = BUFFER_SIZE;
+const unsigned int sincF1Length = BUFFER_SIZE;
+const unsigned int sincF2Length = BUFFER_SIZE;
+volatile float inputs[inputsLength] = {};
+volatile float sincF1[sincF1Length] = {};
+volatile float sincF2[sincF2Length] = {};
 
 void setup() {
   Serial.begin(115200);
 
-  float bandwidth = 1;
-  float targetFreq1 = 7;
-  float targetFreq2 = 10;
-  sincFilter(sincF1, BUFFER_SIZE, samplingFrequency, bandwidth, targetFreq1);
-  sincFilter(sincF2, BUFFER_SIZE, samplingFrequency, bandwidth, targetFreq2);
+  const float bandwidth = 1;
+  const float targetFreq1 = 7;
+  const float targetFreq2 = 10;
+  sincFilter(sincF1, sincF1Length, samplingFrequency, bandwidth, targetFreq1);
+  sincFilter(sincF2, sincF2Length, samplingFrequency, bandwidth, targetFreq2);
 }
 
 void loop() {
   unsigned long currentTimestamp = isMicrosecond ? micros() : millis();
-  if (currentTimestamp - previousRead >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / samplingFrequency) {
-    previousRead = currentTimestamp;
-    int input = analogRead(VOLUME);
+  if (currentTimestamp - lastTimestamp >= (isMicrosecond ? MICROSECONDS : MILLISECONDS) / samplingFrequency) {
+    lastTimestamp = currentTimestamp;
+    unsigned int input = analogRead(VOLUME);
     float sampling = ((1024 * 0.5 - 1) - input) / 1024;
     Serial.println(sampling);
 
-    for (int i = 0; i < (sizeof(inputs) / sizeof(float)) - 1; i++) { 
+    for (unsigned int i = 0; i < inputsLength - 1; i++) { 
       inputs[i] = inputs[i + 1];
     }
-    inputs[(sizeof(inputs) / sizeof(float)) - 1] = sampling;
+    inputs[inputsLength - 1] = sampling;
 
-    bool isSame = true;
-    int convF1Length = isSame ? inputsLength : inputsLength + sincF1Length - 1 ;
-    float convF1[convF1Length] = {};
-    int convF2Length = isSame ? inputsLength : inputsLength + sincF2Length - 1 ;
-    float convF2[convF2Length] = {};
+    const bool isSame = true;
+    unsigned int convF1Length = isSame ? inputsLength : inputsLength + sincF1Length - 1 ;
+    volatile float convF1[convF1Length] = {};
+    unsigned int convF2Length = isSame ? inputsLength : inputsLength + sincF2Length - 1 ;
+    volatile float convF2[convF2Length] = {};
     
     convolution(inputs, inputsLength, sincF1, sincF1Length, convF1, isSame);
     convolution(inputs, inputsLength, sincF2, sincF2Length, convF2, isSame);
   }
 }
 
-void sincFilter(float* vector, int vectorSize, float samplingRate, float bandWidth, float shiftFrequency) {
-  for (int i = 0; i < vectorSize; i++) {
+void sincFilter(float vector[], unsigned int vectorSize, float samplingRate, float bandWidth, float shiftFrequency) {
+  for (unsigned int i = 0; i < vectorSize; i++) {
     float timeVector = i / samplingRate;
     float x = bandWidth * (timeVector - (vectorSize / samplingRate * 0.5));
     float sinc;
@@ -64,20 +64,20 @@ void sincFilter(float* vector, int vectorSize, float samplingRate, float bandWid
   }
   
   float summation = 0;
-  for (int i = 0; i < vectorSize; i++) {
+  for (unsigned int i = 0; i < vectorSize; i++) {
     summation = summation + pow(vector[i], 2);
   }
-  for (int i = 0; i < vectorSize; i++) {
+  for (unsigned int i = 0; i < vectorSize; i++) {
     vector[i] = vector[i] / summation;
   }
 }
 
-void convolution(float *u, int uLength, float *v, int vLength, float *w, bool isSame) {
+void convolution(float u[], unsigned int uLength, float v[], unsigned int vLength, float w[], bool isSame) {
   float tmpArr[uLength + vLength - 1] = {};
-  for (int i = 0; i < uLength + vLength - 1; i++) {
-    int index = i;
+  for (unsigned int i = 0; i < uLength + vLength - 1; i++) {
+    unsigned int index = i;
     float tmpVal = 0;
-    for (int j = 0; j < vLength; j++) {
+    for (unsigned int j = 0; j < vLength; j++) {
       if (index >= 0 && index < uLength) {
         tmpVal += u[index] * v[j];
       }
@@ -86,12 +86,12 @@ void convolution(float *u, int uLength, float *v, int vLength, float *w, bool is
     }
   }
   if (isSame) {
-    int start = ceil((vLength - 1) / 2.0);
-    for (int i = 0; i < uLength; i++) {
+    unsigned int start = ceil((vLength - 1) / 2.0);
+    for (unsigned int i = 0; i < uLength; i++) {
       w[i] = tmpArr[start+i]; 
     }
   } else {
-    for (int i = 0; i < uLength + vLength - 1; i++) {
+    for (unsigned int i = 0; i < uLength + vLength - 1; i++) {
       w[i] = tmpArr[i]; 
     }
   }
